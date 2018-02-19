a642185815ff934182a09e47a50246672a44250b
Hannes Muurinen <hannes.muurinen@reaktor.com>
2018-02-19 15:19:35 +0200

    reduced clutter

--- docs/backends.js
+++ docs/backends.js
@@ -43,26 +43,23 @@ function backendOptions() {
       'circle': {
          name: 'Circle CI',
          url: 'https://circleci.com/api/v1/projects',
-         supportsOwnernameFiltering: false,
          token: undefined
       },
       'travis': {
          name: 'Travis CI',
          url: 'https://api.travis-ci.com/repos',
-         supportsOwnernameFiltering: true,
+         ownernameFiltering: true,
          ownername: '',
          token: undefined
       },
       'jenkins': {
          name: 'Jenkins CI',
          url: undefined,
-         supportsOwnernameFiltering: false,
          token: undefined
       },
       'cloudwatch': {
          name: 'AWS CloudWatch',
          url: 'https://monitoring.eu-west-1.amazonaws.com/',
-         supportsOwnernameFiltering: false,
          token: undefined
       }
    }
